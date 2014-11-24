dcdd4359b64072861411efe7f1ce54e145483347
Micky_Mic <mic100@hotmail.fr>
2014-11-24 18:23:50 +0100

    Update bjorg.fr.py

--- bjorg.fr.py
+++ bjorg.fr.py
@@ -20,36 +20,19 @@ urla = ["http://www.bjorg.fr/recettes/crepes-avec-boissons-vegetales.html",
         
 urlb = ["http://www.bjorg.fr/recettes/tofu.html"]
 
-#on dispatch les url pour controler le flux de donnees envoyee au serveur
-#et ne pas etre bannis.
+#dispatch urls 
 urls = tk.dispatch(textfilename="dispatch.txt", listename=urla, ratio=100)  
-
-#on ecrit la liste dans un fichier pour avoir 
-#un backup en cas de plantage du telechergement ou blocage 
-#par le site (etre bannis).
-
 url_file = open("url_file.txt", "w")      
 
 for url in urls :
-    
     print "url : ", url, "\n"
-    
-    #on passe les urls dans curl pour les recuperer.
     pool =tk.curl(url)
-    
-    #on change a chaque fois d 'adresse ip pour ne pas etre bannis.
     tk.oldnew_ipadress()
-    
-    #la liste ou l on va stocker les urls.
     url_list = []
-    
     for c in pool :
-        
         data = c.body.getvalue()
         soup = tk.BeautifulSoup(data)
-    #    print soup
         s = soup.findAll('h2')
-        
         for i in s :
             a = i.findAll('a')
             if a == [] :
@@ -62,17 +45,13 @@ for url in urls :
                 
                 print a
         print '\n'
-         
-    #on ecrit dans le fichier ".txt" les urls.
     for i in url_list :
         url_file.write(i + "\n") 
-
-#on ferme le fichier en ecriture. 
 url_file.close()
 print "\n"
     
 #-----------------------------------------------------------------------------#
-#                         Recuperation url                                    #
+#                               Get urls                                      #
 #-----------------------------------------------------------------------------#
 
 #on re ouvre le fichier en lecture.
@@ -80,7 +59,7 @@ recipe = open("url_file.txt", "r").readlines()
 print "len(recipe) : ", len(recipe)
 
 #-----------------------------------------------------------------------------#
-#                          Recuperation des donnees                           #
+#                               Get Data                                      #
 #-----------------------------------------------------------------------------#
 
 urls = [i.replace('\n', '') for i in recipe]
@@ -89,34 +68,26 @@ urls = tk.dispatch(textfilename="dispatch_niv2.txt", listename=urls, ratio=1)
 
 counta = 0
 for url in urls :
-    
     print "url : ", url
-    
     counta += 1
-
     print counta, "/", len(urls)
-    
     pool = tk.curl(url)
-    
-    #on change d adresse ip apres chaue download de donnees. 
     tk.oldnew_ipadress()
-    
     countb = 0
-    
     for c in pool :
         data = c.body.getvalue()
         soup = tk.BeautifulSoup(data)
         s = soup.findAll('div', {'id' : 'recette'})
     
         #-------------------------------------------------------------------------#
-        #                                 Titre                                   #
+        #                                 Title                                   #
         #-------------------------------------------------------------------------#
         
         title = s[0].findAll('h1')
         title = title[0].text
     
         #-------------------------------------------------------------------------#
-        #                                 Nbr_peron                               #
+        #                                 Nbr_person                              #
         #-------------------------------------------------------------------------#
          
         person = s[0].findAll('div', {'class' : 'infos'})
@@ -126,7 +97,7 @@ for url in urls :
         person = person.replace('s', '')
     
         #-------------------------------------------------------------------------#
-        #                                 Temps_prepa                             #
+        #                                 Temp_prepa                             #
         #-------------------------------------------------------------------------#
     
         prep_time = s[0].findAll('span', {'class' : 'prepTime'})
@@ -144,15 +115,10 @@ for url in urls :
         ingredient = s[0].findAll('div', {'class' : 'ingredient'})
         ingredient = ingredient[0].findAll('div')
         ingredient = ingredient[0].findAll('li')
-    #    ingredient = ingredient[0]
         
-        #liste des items a filtrer dans la loop
-        #suivante.
         avoid_list = ["<br />", ".", "- "]
         
         for i in ingredient :
-            #on evite <br/> en passant par 
-            # la chaine de caractere de "i".
             if str(i) in avoid_list :
                 pass
             else :
@@ -170,7 +136,6 @@ for url in urls :
                 a = a.replace("&acirc;", "a")
                 a = a.replace("<li>", "").replace("</li>", "")
                 a = a.split("<a")[0]
-    #            a = a.replace("</a", "") 
                 print a, ";",
                  
         #-------------------------------------------------------------------------#
